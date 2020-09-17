915055021e9a4e5c19d4c4e08502d549852aaac9
Charlotte Neff <charlotte.neff@gmail.com>
2020-09-17 17:15:34 -0500

    message

---
+++ lib/cli.rb
@@ -0,0 +1,3 @@
+## interactions with user 
+# contain all of our gets and puts 
+# control flow of our program
---
+++ lib/drink.rb
@@ -0,0 +1,3 @@
+# keep track of our drink
+#turn our responses into objects
+#save all drinks created
